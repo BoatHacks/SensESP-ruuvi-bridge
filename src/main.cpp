666811b862c391cd692f53efd487bde48e1e2581
Tobias C. Rosenstock <dev@air-discordia.org>
2023-05-31 21:35:02 +0200

    first working version, still pretty fugly

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,56 @@
 #include "Arduino.h"
 
 #include "sensesp.h"
-#include "sensesp/sensors/analog_input.h"
 #include "sensesp/signalk/signalk_output.h"
-#include "sensesp/transforms/linear.h"
+#include "sensesp/signalk/signalk_metadata.h"
 #include "sensesp_app.h"
 #include "sensesp_app_builder.h"
 #include "ui_configurables.h"
 
-// #include <StringSplitter.h>
+#include <StringSplitter.h>
 
 #include <NimBLEDevice.h>
 
-
 using namespace sensesp;
 
-// StringConfig *conf_whitelist;
-// StringSplitter *split_whitelist;
+StringConfig *conf_allowlist;
+StringSplitter *split_allowlist;
+
+#define NUM_TAGS 5
+SKOutputFloat* temperatureOut[NUM_TAGS];
+SKOutputFloat* humidityOut[NUM_TAGS];
+SKOutputFloat* pressureOut[NUM_TAGS];
+SKOutputFloat* voltageOut[NUM_TAGS];
+SKOutputInt* signalOut[NUM_TAGS];
+std::string maclist[NUM_TAGS];
 
 NimBLEScan* pBLEScan;
 
-void printWhitelist();
-void decodeV3(const char* macaddr, int signal, const char* data);
-void decodeV5(const char* macaddr, int signal, const char* data);
-
+void printAllowlist();
+void decodeV3(const char* macaddr, const int signal, const char* data);
+void decodeV5(const char* macaddr, const int signal, const char* data);
+void constructSKOutput(const char* macaddr, const int idx);
 
 class MyAdvertisedDeviceCallbacks: public NimBLEAdvertisedDeviceCallbacks {
     void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
       if (advertisedDevice->getManufacturerData()[0] == 0x99 && advertisedDevice->getManufacturerData()[1] == 0x04) {
-/*
-        debugI("FOUND RUUVI TAG: %s - %s\n",
-                std::string(advertisedDevice->getAddress()).c_str(),
-                std::string(advertisedDevice->getManufacturerData()).c_str());
-*/ 
-        debugI("FOUND RUUVI TAG: %s\n", advertisedDevice->toString().c_str());
+        debugI("found ruuvi-tag: %s\n", advertisedDevice->toString().c_str());
       }
 
-//      if (advertisedDevice->getManufacturerData()[2] == 3) {
-//        decodeV3();
-//      }
+      if (advertisedDevice->getManufacturerData()[2] == 3) {
+        char data[25];
+        for (uint8_t i = 0; i < sizeof(advertisedDevice->getManufacturerData()); i++){
+          data[i] = advertisedDevice->getManufacturerData()[i];
+        }
+        decodeV3( advertisedDevice->getAddress().toString().c_str(),
+                  advertisedDevice->getRSSI(),
+                  data);
+      }
       if (advertisedDevice->getManufacturerData()[2] == 5) {
         char data[25];
         for (uint8_t i = 0; i < sizeof(advertisedDevice->getManufacturerData()); i++){
           data[i] = advertisedDevice->getManufacturerData()[i];
         }
-
         decodeV5( advertisedDevice->getAddress().toString().c_str(),
                   advertisedDevice->getRSSI(),
                   data);
@@ -67,27 +73,9 @@ void setup() {
   // Create the global SensESPApp() object.
   SensESPAppBuilder builder;
   sensesp_app = builder.set_hostname("SensESP-ruuvi-bridge")
+                    ->enable_system_info_sensors()
                     ->enable_ota("thisisfine")
                     ->get_app();
-
-//  app.onRepeat(2000, []() {printWhitelist();});
-
-/*
- *  CONFIG_BTDM_SCAN_DUPL_TYPE_DATA_DEVICE (2)
- *  Filter by address and data, advertisements from the same address will be reported only once,
- *  except if the data in the advertisement has changed, then it will be reported again.
-*/
-//  NimBLEDevice::setScanFilterMode(CONFIG_BTDM_SCAN_DUPL_TYPE_DEVICE);
-
-/** *Optional* Sets the scan filter cache size in the BLE controller.
- *  When the number of duplicate advertisements seen by the controller
- *  reaches this value it will clear the cache and start reporting previously
- *  seen devices. The larger this number, the longer time between repeated
- *  device reports. Range 10 - 1000. (default 20)
- *
- *  Can only be used BEFORE calling NimBLEDevice::init.
- */
-  // NimBLEDevice::setScanDuplicateCacheSize(200);
 
   NimBLEDevice::init("");
 
@@ -100,22 +88,20 @@ void setup() {
   pBLEScan->setMaxResults(0); // do not store the scan results, use callback only.
   pBLEScan->setFilterPolicy(BLE_HCI_SCAN_FILT_NO_WL);
 
-/*
-  conf_whitelist = new StringConfig("aa:bb:cc:dd:ee:ff,11:22:33:44:55:66", "/Settings/Whitelist", 
-    "comma-separated list (NO SPACES!) of the MAC addresses of your ruuvi-Tags", 1000 );
+  conf_allowlist = new StringConfig("", "/Settings/Allowlist", 
+    "comma-separated list (NO SPACES!) of the MAC addresses (in lowercase) of your ruuvi-tags", 100 );
 
-  debugD("whitelist: %s\n", conf_whitelist->get_value().c_str());
+  debugD("allowlist: %s\n", conf_allowlist->get_value().c_str());
 
-  split_whitelist = new StringSplitter(conf_whitelist->get_value(), ',', 15);
+  split_allowlist = new StringSplitter(conf_allowlist->get_value(), ',', 15);
 
-  int itemCount = split_whitelist->getItemCount();
+  int itemCount = split_allowlist->getItemCount();
   for(int i = 0; i < itemCount; i++){
-    String item = split_whitelist->getItemAtIndex(i);
+    String item = split_allowlist->getItemAtIndex(i);
     debugD("split item: --%s--\n", item.c_str());
-    std::string macaddr(item.c_str());
-    NimBLEDevice::whiteListAdd(macaddr);
+    constructSKOutput(item.c_str(), i);
+    maclist[i] = item.c_str();
   }
-*/
 
   // Start the SensESP application running
   sensesp_app->start();
@@ -135,50 +121,78 @@ void loop() {
   app.tick(); 
 }
 
-void printWhitelist() {
-  Serial.println("Whitelist contains:");
-  for (auto i=0; i<NimBLEDevice::getWhiteListCount(); ++i) {
-    Serial.println(NimBLEDevice::getWhiteListAddress(i).toString().c_str());
-  }
-};
-
-void decodeV3(const char* macaddr, int signal, const char* data) {
+void decodeV3(const char* macaddr, const int signal, const char* data) {
 
     double temperature = data[4] & 0b01111111;
     temperature = temperature + ((double)data[5] / 100);
     if (data[4] & 0b10000000 == 0) {temperature = temperature * -1;}
     double pressure = ((unsigned short)data[6]<<8) + (unsigned short)data[7] + 50000;
-    short accelX  = ((unsigned short)data[8]<<8)  +  (unsigned short)data[9];
-    short accelY  = ((unsigned short)data[10]<<8) +  (unsigned short)data[11];
-    short accelZ  = ((unsigned short)data[12]<<8) +  (unsigned short)data[13];
-    short voltage = ((unsigned short)data[14]<<8) +  (unsigned short)data[15];
+    //  measurement intervals are too long to be of any use, save some memory and not even parse acceleration
+    short voltage = ((unsigned short)data[14]<<8) + (unsigned short)data[15];
 
-//    debugD(fmt3, temperature,(double)data[3]/2, pressure,
-//            (double)accelX/1000,(double)accelY/1000,(double)accelZ/1000,double(voltage)/1000,signal);
+    debugD("v3: %s - t:%02.1f h:%02.1f p:%06.f v:%02.2f s:%d\n",
+            temperature,
+            (double)data[3]/2,
+            pressure,
+            double(voltage)/1000,
+            signal);
+
+    for (int i=0; i < NUM_TAGS; i++) {
+      if ((String)maclist[i].c_str() == (String)macaddr) {
+        debugI("found %s in allowlist at index %i", maclist[i].c_str(), i);
+        temperatureOut[i]->set_input(temperature + 272.15);
+        humidityOut[i]->set_input(((double)data[3]/2)/100);
+        pressureOut[i]->set_input(pressure);
+        voltageOut[i]->set_input(double(voltage)/1000);
+        signalOut[i]->set_input(signal);
+      }
+    }
+
 }
 
-void decodeV5(const char* macaddr, int signal, const char* data) {
+void decodeV5(const char* macaddr, const int signal, const char* data) {
     short temperature = ((short)data[3]<<8) | (unsigned short)data[4];
     unsigned short humidity = ((unsigned short)data[5]<<8) | (unsigned short)data[6];
-    float pressure    = ((unsigned short)data[7]<<8)  + (unsigned short)data[8] + 50000;
-    
-    short accelX  = ((short)data[9]<<8)  | (short)data[10];
-    short accelY  = ((short)data[11]<<8) | (short)data[12];
-    short accelZ  = ((short)data[13]<<8) | (short)data[14];
-    
-    unsigned short foo = ((unsigned short)data[15] << 8) + (unsigned short)data[16];
-    float voltage = ((double)foo / 32  + 1600)/1000;
-    short power = (((data[16] & 0x1f)*2)-40);
-    unsigned short seqnumber = ((unsigned short)data[18] << 8) + (unsigned short)data[19];
+    float pressure = ((unsigned short)data[7]<<8)  + (unsigned short)data[8] + 50000;
+    //  measurement intervals are too long to be of any use, save some memory and not even parse acceleration
+    unsigned short extra = ((unsigned short)data[15] << 8) + (unsigned short)data[16];
+    float voltage = ((double)extra / 32 + 1600)/1000;
 
-    debugD("%s - t:%02.1f h:%02.1f p:%04.2f aX:%03.3f aY:%03.3f aZ:%03.3f v:%02.2f s:%d\n",
+    debugD("%s - t:%02.1f h:%02.1f p:%06.f v:%02.2f s:%d\n",
           macaddr,
           (float)temperature*0.005,
           (unsigned int)humidity*0.0025,
-          pressure/100,
-          (float)accelX/1000,
-          (float)accelY/1000,
-          (float)accelZ/1000,
+          pressure,
           voltage,
           signal);
+
+    for (int i=0; i < NUM_TAGS; i++) {
+      if ((String)maclist[i].c_str() == (String)macaddr) {
+        debugI("found %s in allowlist at index %i", maclist[i].c_str(), i);
+        temperatureOut[i]->set_input((float)temperature*0.005 + 272.15);
+        humidityOut[i]->set_input(((unsigned int)humidity*0.0025)/100);
+        pressureOut[i]->set_input(pressure);
+        voltageOut[i]->set_input(voltage);
+        signalOut[i]->set_input(signal);
+      }
+    }
+}
+
+void constructSKOutput(const char* macaddr, const int idx) {
+  temperatureOut[idx] = new SKOutputFloat("environment." + (String)macaddr + ".temperature",
+                                            "/Paths/" + (String)macaddr + "/Temperature",
+                                            "K");
+  humidityOut[idx] = new SKOutputFloat("environment." + (String)macaddr + ".humidity",
+                                            "/Paths/" + (String)macaddr + "/Humidity",
+                                            "Ratio");
+  pressureOut[idx] = new SKOutputFloat("environment." + (String)macaddr + ".pressure",
+                                            "/Paths/" + (String)macaddr + "/Pressure",
+                                            "Pa");
+  voltageOut[idx] = new SKOutputFloat("sensorDevice.ruuvi-" + (String)macaddr + ".voltage",
+                                            "/Paths/" + (String)macaddr + "/Voltage",
+                                            "V");
+  signalOut[idx] = new SKOutputInt("sensorDevice.ruuvi-" + (String)macaddr + ".signal",
+                                            "/Paths/" + (String)macaddr + "/Signal",
+                                            "dBm");
+  debugI("created SK connections for %s, index %d", macaddr, idx);
 }
